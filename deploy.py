dba15f2a720337454ad66fbaa5323e16c169a978
Douwe van Gijn <douwevangijn@hotmail.com>
2023-06-22 22:30:41 +0100

    pretty printing and deleting remote files

--- deploy.py
+++ deploy.py
@@ -12,11 +12,12 @@ def main():
     s3 = authenticate()
     objects = list_objects(s3)
 
-    max_filename_len = max([len(key) for key in objects])
+    local_files = list(local_path.iterdir())
+    local_file_names = [str(p.name) for p in local_files]
+    max_filename_len = max([len(p) for p in list(objects.keys())+local_file_names])
 
-    for file in local_path.iterdir():
-        fname = (ceil(max_filename_len/2)*'· ')[:max_filename_len]
-        fname = file.name + fname[len(file.name):] + ' '
+    for file in local_files:
+        fname = pad_with_dots(file.name, max_filename_len)
         warn = '⚠️'+'\033[4G'+fname
         upld = '⬆️'+'\033[4G'+fname
         keep = '✅'+'\033[4G'+fname
@@ -29,7 +30,7 @@ def main():
             print(f'{upld}different size on remote -> uploading')
             put_object(s3, file)
         else:
-            head = s3.head_object(Bucket='www', Key=file.name)
+            head = head_object(s3, file)
             if 'sha3-256' not in head['Metadata']:
                 print(f'{upld}missing sha3-256 hash -> uploading')
                 put_object(s3, file)
@@ -39,6 +40,19 @@ def main():
             else:
                 print(f'{keep}hash matches -> skipping')
 
+    extra_remote_files = list(set(objects.keys()) - set(local_file_names))
+    if len(extra_remote_files) == 0:
+        print('✨'+'\033[4G'+'remote is clean')
+    else:
+        st = []
+        for name in extra_remote_files:
+            st.append('🚮'+'\033[4G'+pad_with_dots(name, max_filename_len)+'deleting extra remote file')
+        print('\n'.join(st))
+        delete_objects(s3, extra_remote_files)
+
+    print_opcount()
+
+
 def authenticate():
     secrets_path =  Path(__file__).parent/".secrets"
     return boto3.client('s3',
@@ -47,7 +61,21 @@ def authenticate():
         endpoint_url = f"https://{(secrets_path/'account_id').read_text(encoding='utf-8').strip()}.r2.cloudflarestorage.com",
         region_name = 'auto')
 
+def head_object(s3, file:Path):
+    inc_opcount('headObject','B')
+    return s3.head_object(Bucket='www', Key=file.name)
+
+def delete_objects(s3, names:list[str]):
+    if len(names) > 0:
+        inc_opcount('deleteObjects','0')
+        s3.delete_objects(
+            Bucket='www',
+            Delete={
+                'Objects': [{'Key':n} for n in names],
+                'Quiet': True })
+
 def list_objects(s3):
+    inc_opcount('listObjectsV2','A')
     response = s3.list_objects_v2(Bucket='www')
     if response['IsTruncated']: exit('truncated listObjectsV2 response not yet supported')
     files = {}
@@ -62,13 +90,57 @@ def hash(file_bytes:bytes):
 
 def put_object(s3, filepath:Path):
     file_bytes = filepath.read_bytes()
-    response = s3.put_object(
+    inc_opcount('putObject','A')
+    s3.put_object(
         Key=filepath.name,
         Body=file_bytes,
         Bucket='www',
         Metadata={
             'sha3-256': hash(file_bytes)
         })
+
+def pad_with_dots(s:str, max_len:int):
+    dots = (ceil(max_len/2)*'· ')[:max_len]
+    return s + dots[len(s):] + ' '
+
+opcount = { }
+
+def inc_opcount(op:str, opclass:str):
+    idx = {'A':0,'B':1,'0':2}[opclass];
+    if op not in opcount:
+        opcount[op] = [0,0,0]
+    opcount[op][idx] += 1
+
+def boxify(lines:list[str]):
+    wid = len(lines[0])
+    ret = []
+    ret.append('╭─' + wid*'─'  + '─╮')
+    ret.append('│ ' + lines[0] + ' │')
+    ret.append('├╶' + wid*'╶'  + '╶│')
+    for line in lines[1:-1]:
+        ret.append('│ ' + line + ' │')
+    ret.append('├╶' + wid*'╶'  + '╶│')
+    ret.append('│ ' + lines[-1]+ ' │')
+    ret.append('╰─' + wid*'─'  + '─╯')
+    return ret
+
+
+def print_opcount():
+    max_op_len = max([len(key) for key in opcount])
+    lines = []
+    lines.append('op'.ljust(max_op_len) + '    A    B    0')
+    total = [0,0,0]
+    for k,v in opcount.items():
+        line = k.ljust(max_op_len)
+        for i,n in enumerate(v):
+            line += '    ·' if n==0 else f'{n:>5}'
+            total[i] += n
+        lines.append(line)
+    line = 'total'.ljust(max_op_len)
+    for t in total:
+        line += f'{t:>5}'
+    lines.append(line);
+    print('\n'.join(boxify(lines)))
     
 if __name__ == "__main__":
     main()
